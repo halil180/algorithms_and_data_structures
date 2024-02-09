8460300f7cc0942a0e7833a2a276820f88b3d97b
halil180 <halilkom180@.com>
2024-02-09 19:08:17 +0100

    built-in binary search

--- src/main/java/searching/binary/Main.java
+++ src/main/java/searching/binary/Main.java
@@ -1,10 +1,13 @@
 package searching.binary;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //for binary search to work at all, the values have to be IN ORDER
         int[] nums = {4,6,7,12,34,66,77,86};
         System.out.println(binarySearch(nums,66));
+        System.out.println(Arrays.binarySearch(nums,66));
     }
 
     private static int binarySearch(int[] numbers,int numberToFind){
