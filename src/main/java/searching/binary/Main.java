f646c3e55ce21fd41b7d7443b68b3140d65613a7
halil180 <halilkom180@.com>
2024-02-09 19:03:55 +0100

    binary search

---
+++ src/main/java/searching/binary/Main.java
@@ -0,0 +1,31 @@
+package searching.binary;
+
+public class Main {
+    public static void main(String[] args) {
+        //for binary search to work at all, the values have to be IN ORDER
+        int[] nums = {4,6,7,12,34,66,77,86};
+        System.out.println(binarySearch(nums,66));
+    }
+
+    private static int binarySearch(int[] numbers,int numberToFind){
+
+        int low = 0;
+        int high = numbers.length - 1;
+
+        while (low <= high){
+         int middlePostion = (low + high) / 2;
+         int middleNumber = numbers[middlePostion];
+
+         if (middleNumber == numberToFind){
+             return  middlePostion;
+         }
+
+            if (numberToFind < middleNumber){
+                high = middlePostion - 1;
+            }else{
+                low = middlePostion + 1;
+            }
+        }
+        return -1;
+    }
+}
